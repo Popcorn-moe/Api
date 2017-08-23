a3ef8ecf11a768500c350f3ebbac1fa5219c0e43
SkyBeastMC <skybeastmc@gmail.com>
2017-08-24 00:01:31 +0200

    Queries

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-import schema from './src/schema'
-import { join } from 'path'
-import { inspect } from 'util'
-import { graphql, introspectionQuery } from 'graphql'
+import schema from './src/schema';
+import { join } from 'path';
+import { inspect } from 'util';
+import { graphql } from 'graphql';
+import { MongoClient } from 'mongodb';
 
-graphql(schema, '{ me { email login id }}', { message: 'hello' }).then(res => console.log(JSON.stringify(res, null, 2)))
+const url = 'mongodb://localhost:27017/popcornmoe_backend';
+
+MongoClient.connect(url)
+           .then(db =>
+                 {
+                     graphql(schema, '{ me { email login id }}', { db })
+                         .then(res => console.log(JSON.stringify(res, null, 2)));
+                 });
--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -3,32 +3,107 @@
 type ID = string
 type Context = any
 
-export function me(context: Context): User {
-    return {
-        id: "abc",
-        login: "example",
-        email: "example",
-        group: 1,
-        newsletter: false,
-        account_registered: true,
-        ratings: [],
-        rating_for: null
-    }
-}
-export function animes(context: Context, { limit, sort } : { limit: number, sort: Sort }): ?Array<Anime> {
-    return null;
-}
-export function author(context: Context, { id } : { id : ID }): ?Author {
-    return null;
-}
-export function anime(context: Context, { id } : { id : ID }): ?Anime {
-    return null;
-}
-export function tag(context: Context, { id } : { id : ID }): ?Tag {
-    console.log(id)
-    return null;
+export function me(context: Context): User
+{
+    let id = 1; //todo
+    return context.db.collection('users')
+                  .findOne({ id })
+                  .then((obj: User) =>
+                        {
+                            if (!obj) return null;
+                            let {
+                                    id, login, email, group, newsletter,
+                                    account_registered, ratings, rating_for
+                                } = obj;
 
+                            return {
+                                id, login, email, group, newsletter,
+                                account_registered, ratings, rating_for
+                            };
+                        });
 }
-export function media(context: Context, { id } : { id : ID }): ?Media {
-    return null;
+
+
+export function animes(context: Context, { limit, sort }: { limit: number, sort: Sort }): ?Array<Anime>
+{
+    return context.db.collection('animes')
+                  .find({ id })
+                  .limit(limit)
+                  .toArray()
+                  .map(
+                      ({
+                           id, names, authors, tags, status, medias,
+                           season, release_date, edit_date, posted_date
+                       }: Anime) =>
+                      {
+                          return {
+                              id, names, authors, tags, status, medias,
+                              season, release_date, edit_date, posted_date
+                          };
+                      }
+                  ); //todo: sort by which field?
+}
+
+export function author(context: Context, { id }: { id: ID }): ?Author
+{
+    return context.db.collection('authors')
+                  .findOne({ id })
+                  .then((obj: Author) =>
+                        {
+                            if (!obj) return null;
+                            let { id, name, picture, bio, organisation, animes } = obj;
+
+                            return { id, name, picture, bio, organisation, animes };
+                        });
+}
+
+export function anime(context: Context, { id }: { id: ID }): ?Anime
+{
+    return context.db.collection('animes')
+                  .findOne({ id })
+                  .then((obj: Anime) =>
+                        {
+                            if (!obj) return null;
+                            let {
+                                    id, names, authors, tags, status, medias,
+                                    season, release_date, edit_date, posted_date
+                                } = obj;
+
+                            return {
+                                id, names, authors, tags, status, medias,
+                                season, release_date, edit_date, posted_date
+                            };
+                        });
+}
+
+export function tag(context: Context, { id }: { id: ID }): ?Tag
+{
+    return context.db.collection('tags')
+                  .findOne({ id })
+                  .then((obj: Tag) =>
+                        {
+                            if (!obj) return null;
+                            let { id, name, desc } = obj;
+
+                            return { id, name, desc };
+                        });
+}
+
+export function media(context: Context, { id }: { id: ID }): ?Media
+{
+    return context.db.collection('medias')
+                  .findOne({ id })
+                  .then((obj: Media) =>
+                        {
+                            if (!obj) return null;
+                            let {
+                                    id, comments, type, rate, release_date,
+                                    edit_date, posted_date
+                                } = obj;
+
+                            return {
+                                id, comments, type, rate, release_date,
+                                edit_date, posted_date
+                            };
+                        });
 }
