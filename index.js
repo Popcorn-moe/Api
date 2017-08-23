af7a70fac35d4328fee74bdbd8df67a518d40483
Duarte David <deltaduartedavid@gmail.com>
2017-08-24 00:40:51 +0200

    Add monitor

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-import schema from './src/schema';
-import { join } from 'path';
-import { inspect } from 'util';
-import { graphql } from 'graphql';
+import schema from './src/schema'
+import { instrument, report, createContext } from './src/monitor'
+import express from 'express'
+import graphqlHTTP from 'express-graphql'
+import logger from 'morgan'
 import { MongoClient } from 'mongodb';
 
-const url = 'mongodb://localhost:27017/popcornmoe_backend';
+instrument(schema)
 
-MongoClient.connect(url)
-           .then(db =>
-                 {
-                     graphql(schema, '{ me { email login id }}', { db })
-                         .then(res => console.log(JSON.stringify(res, null, 2)));
-                 });
+const url = 'mongodb://localhost:27017/popcornmoe_backend';
+const app = express()
+
+app.use(logger('dev'))
+MongoClient.connect(url).then(db =>{
+    const graphql = graphqlHTTP({
+        schema,
+        rootValue: { db },
+        graphiql: true
+    });
+
+    app.use('/graphql', (req, res, next) => {
+        Object.assign(req, createContext())
+        graphql(req, res, next)
+        res.on('finish', () => report(req.instrument, res._startAt, req._startAt))
+    });
+
+    console.log('Connected on mongodb')
+});
+
+app.listen(3030, () => console.log('Listening on port 3030'))
--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -7,103 +7,81 @@ export function me(context: Context): User
 {
     let id = 1; //todo
     return context.db.collection('users')
-                  .findOne({ id })
-                  .then((obj: User) =>
-                        {
-                            if (!obj) return null;
-                            let {
-                                    id, login, email, group, newsletter,
-                                    account_registered, ratings, rating_for
-                                } = obj;
-
-                            return {
-                                id, login, email, group, newsletter,
-                                account_registered, ratings, rating_for
-                            };
-                        });
+            .find({ id })
+            .limit(1)
+            .map(({
+                    id, login, email, group, newsletter,
+                    account_registered, ratings, rating_for
+                }: User) => ({
+                    id, login, email, group, newsletter,
+                    account_registered, ratings, rating_for
+            }))
+            .next()
 }
 
 
 export function animes(context: Context, { limit, sort }: { limit: number, sort: Sort }): ?Array<Anime>
 {
     return context.db.collection('animes')
-                  .find({ id })
-                  .limit(limit)
-                  .toArray()
-                  .map(
-                      ({
-                           id, names, authors, tags, status, medias,
-                           season, release_date, edit_date, posted_date
-                       }: Anime) =>
-                      {
-                          return {
-                              id, names, authors, tags, status, medias,
-                              season, release_date, edit_date, posted_date
-                          };
-                      }
-                  ); //todo: sort by which field?
+            .find()
+            .limit(limit)
+            .sort(sort === 'NONE' ? {} : {
+                name: sort === 'ASC' ? 1 : -1
+            })
+            .map(({
+                    id, names, authors, tags, status, medias,
+                    season, release_date, edit_date, posted_date
+                }: Anime) => ({
+                    id, names, authors, tags, status, medias,
+                    season, release_date, edit_date, posted_date
+                })
+            )
 }
 
 export function author(context: Context, { id }: { id: ID }): ?Author
 {
     return context.db.collection('authors')
-                  .findOne({ id })
-                  .then((obj: Author) =>
-                        {
-                            if (!obj) return null;
-                            let { id, name, picture, bio, organisation, animes } = obj;
-
-                            return { id, name, picture, bio, organisation, animes };
-                        });
+            .find({ id })
+            .limit(1)
+            .map(({ id, name, picture, bio, organisation, animes }: Author) =>
+                ({ id, name, picture, bio, organisation, animes }))
+            .next()
 }
 
 export function anime(context: Context, { id }: { id: ID }): ?Anime
 {
     return context.db.collection('animes')
-                  .findOne({ id })
-                  .then((obj: Anime) =>
-                        {
-                            if (!obj) return null;
-                            let {
-                                    id, names, authors, tags, status, medias,
-                                    season, release_date, edit_date, posted_date
-                                } = obj;
-
-                            return {
-                                id, names, authors, tags, status, medias,
-                                season, release_date, edit_date, posted_date
-                            };
-                        });
+            .find({ id })
+            .limit(1)
+            .map(({
+                    id, names, authors, tags, status, medias,
+                    season, release_date, edit_date, posted_date
+                }: Anime) => ({
+                        id, names, authors, tags, status, medias,
+                        season, release_date, edit_date, posted_date
+            }))
+            .next()
 }
-
 export function tag(context: Context, { id }: { id: ID }): ?Tag
 {
     return context.db.collection('tags')
-                  .findOne({ id })
-                  .then((obj: Tag) =>
-                        {
-                            if (!obj) return null;
-                            let { id, name, desc } = obj;
-
-                            return { id, name, desc };
-                        });
+            .find({ id })
+            .limit(1)
+            .map(({ id, name, desc }: Tag) => ({ id, name, desc }))
+            .next()
 }
 
 export function media(context: Context, { id }: { id: ID }): ?Media
 {
     return context.db.collection('medias')
-                  .findOne({ id })
-                  .then((obj: Media) =>
-                        {
-                            if (!obj) return null;
-                            let {
-                                    id, comments, type, rate, release_date,
-                                    edit_date, posted_date
-                                } = obj;
-
-                            return {
-                                id, comments, type, rate, release_date,
-                                edit_date, posted_date
-                            };
-                        });
+            .find({ id })
+            .limit(1)
+            .map(({
+                    id, comments, type, rate, release_date,
+                    edit_date, posted_date
+                } : Media) => ({
+                    id, comments, type, rate, release_date,
+                    edit_date, posted_date
+            }))
+            .next()
 }
