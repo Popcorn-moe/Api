50e0192033416d81362a994a8e0da7a905c072ad
Duarte David <deltaduartedavid@gmail.com>
2017-10-24 15:54:33 +0200

    Setup unit tests

---
+++ test/unit/resolvers/util/needAuth.test.js
@@ -0,0 +1,14 @@
+import needAuth from '../../../../src/resolvers/util/needAuth'
+
+describe('needGroup', () => {
+	describe('given no user', () => {
+		test('throw Error', () => {
+			expect(() => needAuth({})).toThrow()
+		})
+	})
+	describe('given an user', () => {
+		test('return', () => {
+			expect(needAuth({ user: Promise.resolve() }))
+		})
+	})
+})
---
+++ test/unit/resolvers/util/needGroup.test.js
@@ -0,0 +1,28 @@
+import needGroup from '../../../../src/resolvers/util/needGroup'
+import { ADMIN, MODERATOR, VIEWER } from '../../../../src/resolvers/util/groups'
+
+describe('needGroup', () => {
+	const USER = { group: MODERATOR }
+	let req
+	beforeEach(() => {
+		req = {
+			user: Promise.resolve(USER)
+		}
+	})
+	describe('given an unknown group', () => {
+		test('throw Error', () => {
+			expect(() => needGroup(req, 'unicorn')).toThrow()
+		})
+	})
+	describe('given a group', () => {
+		test('return user if group is valid', () => {
+			return needGroup(req, VIEWER).then(res => expect(res).toBe(USER))
+		})
+		test('throw Error if group is invalid', () => {
+			return needGroup(req, ADMIN).then(
+				res => expect(res).toBeUndefined(),
+				err => expect(err).toBeDefined()
+			)
+		})
+	})
+})
