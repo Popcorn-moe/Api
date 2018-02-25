0f14e753399e4a5df37d28f458fc57e9920d66ea
LightDiscord <arnaud@lightdiscord.me>
2018-02-25 22:18:12 +0100

    Default avatar using Gravatar

--- src/resolvers/query/User.js
+++ src/resolvers/query/User.js
@@ -1,5 +1,6 @@
 import { ObjectID } from "mongodb";
 import { getNotifications } from "./query";
+import md5 from "md5";
 
 export function playlists(root, args, context) {
 	return context.db
@@ -29,4 +30,11 @@ export function blocked(root, args, context) {
 
 export function notifications(root, args, context) {
 	return getNotifications(null, { user: root.id }, context);
+}
+
+export function avatar({ avatar, email }, args, context) {
+	return (
+		avatar ||
+		`https://www.gravatar.com/avatar/${md5(email.toLowerCase().trim())}`
+	);
 }
