198d185fd1106ccff194ecc5b18c78d42d7aea30
Duarte David <deltaduartedavid@gmail.com>
2018-04-02 11:26:48 +0200

    Don't throw error for user email

--- src/directives/auth.js
+++ src/directives/auth.js
@@ -5,18 +5,27 @@ const GROUPS = ["VIEWER", "MODERATOR", "ADMIN"];
 
 export default class AuthDirective extends SchemaDirectiveVisitor {
 	visitFieldDefinition(field) {
-		const { requires } = this.args;
+		const { requires, error } = this.args;
 		const { resolve = defaultFieldResolver } = field;
 		field.resolve = function(root, args, context, info) {
-			if (!context.user) throw new Error("User not authenticated");
+			if (!context.user) {
+				if (error) throw new Error("User not authenticated");
+				else return null;
+			}
 
 			const index = GROUPS.indexOf(requires);
-			if (index === -1)
-				throw new Error(`Group ${requires} not in ${GROUPS.join(",")}`);
+			if (index === -1) {
+				if (error)
+					throw new Error(`Group ${requires} not in ${GROUPS.join(",")}`);
+				else return null;
+			}
 			return context.user.then(user => {
 				if (GROUPS.indexOf(user.group) >= index)
 					return resolve.call(this, root, args, context, info);
-				else return Promise.reject(`Need group ${group}`);
+				else {
+					if (error) return Promise.reject(`Need group ${group}`);
+					else return Promise.resolve(null);
+				}
 			});
 		};
 	}
